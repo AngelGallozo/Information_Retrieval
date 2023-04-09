0336b978449aabb4d64e0483bf3d1a29c6b1010c
AngelGallozo <luisangelgallozo@gmail.com>
2023-04-08 21:49:09 -0300

    Finalizacion Punto 8

--- TP1/punto_8.py
+++ TP1/punto_8.py
@@ -1,5 +1,10 @@
-list_terms={}
+import sys
+import re
+import os
+from os import listdir
+from os.path import join, isdir
 
+list_terms={}
 
 regex_alpha_words = re.compile(r'[^a-zA-Z0-9]') # Cadenas alfanumericas sin acentos
 # Extrae los tokens en una lista
@@ -14,24 +19,32 @@ def tokenizer(line):
 
 
 def main():
-    with open("texto.txt",'r',encoding='utf-8') as f:
-        for line in f:
-            terms_list = tokenizer(line)
-            for term in terms_list:
-                if term in list_terms:
-                    list_terms[term]+=1
-                else:
-                    list_terms[term]= 1   
-
-
-
-
-
-
-
-
-
-
+    if len(sys.argv) < 2:
+        print('Es necesario pasar nombre del directorio')        
+        sys.exit(0)
+    dirname = sys.argv[1]
+    
+    if (isdir(dirname)):
+        arch_salida = open(f'punto_8_heaps.txt', "x",encoding='utf-8')
+        terms_total = 0
+        terms_uniq = 0
+        for filename in listdir(dirname):
+            filepath = join(dirname, filename)
+            with open(filepath,'r',encoding='utf-8') as f:
+                print(f"Processing file: {filepath}")
+                for line in f:
+                    terms_list = tokenizer(line)
+                    for term in terms_list:
+                        terms_total += 1
+                        if term in list_terms:
+                            list_terms[term]+=1
+                        else:
+                            list_terms[term]= 1
+                            terms_uniq +=1
+        
+            # Escribimos en el archivo
+            arch_salida.write(str(terms_total)+' '+str(terms_uniq)+"\n")
+        arch_salida.close()
 
 if __name__ == '__main__':
     main()
